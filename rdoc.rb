a8137b860a6fa113a8ffcdb827012aaca9b62087
Scott Taylor <scott@railsnewbie.com>
2008-10-12 14:40:07 -0400

    * Extract out FR specific rdoc title.  Fix analytics project root

--- rdoc.rb
+++ rdoc.rb
@@ -1,5 +1,6 @@
 require 'rake'
 require 'rake/rdoctask'
+require File.dirname(__FILE__) + "/support/set_default"
 
 RDOC_DIRECTORY = "doc"
 
@@ -7,17 +8,20 @@ def doc_directory
   RDOC_DIRECTORY
 end
 
+raise_error_on :rdoc_title
+raise_error_on :project_root
+
 desc 'Generate documentation for the fixture_replacement plugin.'
 Rake::RDocTask.new(:rdoc_without_analytics) do |rdoc|
   rdoc.rdoc_dir = doc_directory
-  rdoc.title    = 'FixtureReplacement'
+  rdoc.title    = rdoc_title
   rdoc.options << '--line-numbers' << '--inline-source'
   rdoc.rdoc_files.include('README')
   rdoc.rdoc_files.include('lib/**/*.rb')
 end
 
 task :rdoc => [:rdoc_without_analytics] do
-  google_analytics = File.read(File.dirname(__FILE__) + "/etc/google_analytics")
+  google_analytics = "#{project_root}/etc/google_analytics"
   rdoc_index = File.dirname(__FILE__) + "/#{doc_directory}/index.html"
   
   contents = File.read(rdoc_index) 
