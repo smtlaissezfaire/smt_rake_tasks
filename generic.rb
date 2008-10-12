407a66bad960ecd1a5c7e5b2b6f9404cb947bc34
Scott Taylor <scott@railsnewbie.com>
2008-10-12 00:29:15 -0400

    * Remove docs task

--- generic.rb
+++ generic.rb
@@ -9,8 +9,3 @@ require "#{path}/specdoc"
 require "#{path}/rspec_rcov"
 require "#{path}/flog"
 require "#{path}/git"
-
-# DOCS - Make this more generic
-
-desc 'Create the specdoc + rdoc'
-task :build_docs => [:rerdoc, :specdoc, :rcov, :flog_to_disk]
