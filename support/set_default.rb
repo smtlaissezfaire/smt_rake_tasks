f4e5e1bf33a7014d461e1ba2ba8cb537c310ee77
Scott Taylor <scott@railsnewbie.com>
2008-10-12 01:23:58 -0400

    * Fix bugs with raise_error_on

--- support/set_default.rb
+++ support/set_default.rb
@@ -1,8 +1,12 @@
 
 def raise_error_on(method_name)
   unless methods.include?(method_name.to_s)
-    define_method(method_name) do
-      raise NotImplementedError, "You will need to define the method #{method_name}"
+    metaclass = class << self; self; end
+    
+    metaclass.instance_eval do
+      define_method(method_name) do
+        raise NotImplementedError, "You will need to define the method #{method_name}"
+      end
     end
   end
 end
